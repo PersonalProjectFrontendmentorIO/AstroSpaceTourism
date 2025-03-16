e8378ad416303a1c40c8a4c93acffd827da8b285
Aldi-H <aldihimawan88@gmail.com>
2025-03-16 15:25:40 +0700

    Refactor Navbar component: enhance layout structure and adjust styling for improved responsiveness

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -19,16 +19,24 @@ const Navbar = () => {
 
 	return (
 		<div className="fixed w-full z-1000 flex justify-between items-center my-10">
-			<div className="flex items-center align-middle">
-				<img src={SpaceTourismLogo} fetchPriority="high" className="mx-16" />
-				<div className="bg-space-white opacity-25 h-px w-[560px] absolute left-0 right-1/6 mx-auto top-1/2 -translate-y-1/2 z-20" />
+			<div className="flex items-center w-full relative">
+				<img
+					src={SpaceTourismLogo}
+					fetchPriority="high"
+					className="mx-16 z-10"
+				/>
+				<div className="bg-space-white opacity-25 h-px w-[80%] absolute -right-10 mx-auto top-1/2 -translate-y-1/2 z-20" />
 			</div>
-			<div className="flex pl-40 pr-16 space-x-12 justify-between backdrop-blur-2xl text-space-white bg-space-white/10 h-24 items-center">
+
+			<div
+				className="flex pl-40 pr-16 space-x-12 justify-between backdrop-blur-2xl text-space-white bg-space-white/10 items-center content-center"
+				style={{ width: "80%", height: "96px" }}
+			>
 				{navItems.map((item, index) => (
 					<a
 						key={item.id}
 						href={item.path}
-						className="space-text-preset-8 flex gap-3 cursor-pointer h-full items-center relative"
+						className="space-text-preset-8 flex gap-3 cursor-pointer h-full items-center align-middle content-center relative"
 					>
 						<div className="flex gap-3 h-full items-center">
 							<div className="font-bold">0{index}</div>
